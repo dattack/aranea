668d486e09af086b0cce44b8d43a9fd471a2980d
cvarela <cvarela@dattack.com>
2017-03-03 18:03:05 +0100

    New method to retrieve the context properties as an AbstractConfiguration object

--- src/main/java/com/dattack/aranea/engine/Context.java
+++ src/main/java/com/dattack/aranea/engine/Context.java
@@ -18,6 +18,7 @@ package com.dattack.aranea.engine;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.apache.commons.configuration.BaseConfiguration;
 
 import com.dattack.jtoolbox.commons.configuration.ConfigurationUtil;
@@ -26,32 +27,39 @@ import com.dattack.jtoolbox.commons.configuration.ConfigurationUtil;
  * @author cvarela
  * @since 0.1
  */
-public class Context {
+public final class Context {
 
     public static final String CURRENT_TIMESTAMP = "current_timestamp";
     public static final String CURRENT_DATE_TIME = "current_datetime";
-    
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss");
-    
+    public static final String CURRENT_DATE = "current_date";
+
+    private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+
     private final BaseConfiguration configuration;
-    
+
     public Context() {
         configuration = new BaseConfiguration();
         configuration.append(ConfigurationUtil.createEnvSystemConfiguration());
-        
-        Date date = new Date();
+
+        final Date date = new Date();
         configuration.setProperty(Context.CURRENT_TIMESTAMP, date.getTime());
-        configuration.setProperty(Context.CURRENT_DATE_TIME, DATE_FORMAT.format(date));
+        configuration.setProperty(Context.CURRENT_DATE_TIME, DATE_TIME_FORMAT.format(date));
+        configuration.setProperty(Context.CURRENT_DATE, DATE_FORMAT.format(date));
     }
-    
+
     public void setProperty(final String key, final Object value) {
         configuration.setProperty(key, value);
     }
-    
+
+    public AbstractConfiguration getConfiguration() {
+        return (BaseConfiguration) configuration.clone();
+    }
+
     public Object getProperty(final String key) {
         return configuration.getProperty(key);
     }
-    
+
     public String interpolate(final Object expression) {
         return ConfigurationUtil.interpolate(expression, configuration);
     }
